85604d020e028831e9a4d0d71800cc0d173211c5
Filip Kotula <fkotula1121@gmail.com>
2022-06-01 14:37:15 +0200

    v1 columns opened work

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,7 +21,9 @@ public class Student {
     @Id
     @SequenceGenerator(name="sequence_student", sequenceName="sequence_student", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_student")
+    @Column(name="id", updatable = false)
     private Long id;
+    @Column(name="first_name", nullable = false, columnDefinition = "TEXT")
     private String firstName;
     private String lastName;
     private String email;
