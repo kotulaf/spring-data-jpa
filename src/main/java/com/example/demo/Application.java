becfdf46b8cf6d4638c5e4ed3b3c56ade80190e1
Filip Kotula <fkotula1121@gmail.com>
2022-06-02 07:56:38 +0200

    v1 repo working

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,12 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository)
+    {
+        return args -> {
+            Student s1 = new Student("Max", "Jones", "max.jones@gmail.com", 21);
+            studentRepository.save(s1);
+        };
+    }
 }
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -7,25 +7,45 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "Student")
-@Table
+@Entity(name = "student")
+@Table(name = "student", uniqueConstraints = {@UniqueConstraint(name = "student_email_unique", columnNames = "email")})
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+
     @Id
     @SequenceGenerator(name="sequence_student", sequenceName="sequence_student", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_student")
+
     @Column(name="id", updatable = false)
     private Long id;
-    @Column(name="first_name", nullable = false, columnDefinition = "TEXT")
+
+    @Column(name="first_name", nullable = false, columnDefinition = "TEXT") // name is text NOT null, therefore we say nullable = false
     private String firstName;
+
+    @Column(name="last_name", nullable = false, columnDefinition = "TEXT")
     private String lastName;
+
+    @Column(name="email", nullable = false, columnDefinition = "TEXT")
     private String email;
+
+    @Column(name="age")
     private Integer age;   
+
+    public Student(String firstName, String lastName, String email, Integer age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
 }
